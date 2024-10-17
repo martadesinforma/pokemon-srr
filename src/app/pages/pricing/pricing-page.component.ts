ade5718138dc8910c973ed47138e8fd347806f56
Marta Ocana <martacyta2021@gmail.com>
2024-10-17 20:18:48 +0200

    create and update pricing page component

---
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -0,0 +1,12 @@
+
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'page-pricing',
+  standalone: true,
+  imports: [
+  ],
+  templateUrl: './pricing-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PricingPageComponent { }
