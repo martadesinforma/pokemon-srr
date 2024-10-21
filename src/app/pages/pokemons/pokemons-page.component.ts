835d974e0355afb54393114c0a76718997354a91
Marta Ocana <martacyta2021@gmail.com>
2024-10-21 18:52:42 +0200

    create and update pokemons component

---
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -0,0 +1,12 @@
+
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'pokemons-page',
+  standalone: true,
+  imports: [
+  ],
+  templateUrl: './pokemons-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PokemonsPageComponent { }
