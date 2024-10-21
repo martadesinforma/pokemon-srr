f7e53fa7caac705a849734631ef3ca798c5796d6
Marta Ocana <martacyta2021@gmail.com>
2024-10-21 18:53:51 +0200

    update navbar.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'pokemons',
+    loadComponent: () => import('./pages/pokemons/pokemons-page.component'), //conseguimos escrbir de manera tan corta la importacion pq en el archivo .ts hemos escrito **default** despues del nombre de la clase. Angular necesita saber dónde en el DOM debe insertar este componente cuando la url sea  http://localhost:52951/pokemons. Aquí es donde entra en juego router-outlet. En este caso lo vamos a insertar en el app.component.html para que se muestre en la pagina principal.
+  },
+  {
     path: 'about',
     loadComponent: () => import('./pages/about/about-page.component'), //conseguimos escrbir de manera tan corta la importacion pq en el archivo .ts hemos escrito **default** despues del nombre de la clase. Angular necesita saber dónde en el DOM debe insertar este componente cuando la url sea  http://localhost:52951/about. Aquí es donde entra en juego router-outlet. En este caso lo vamos a insertar en el app.component.html para que se muestre en la pagina principal.
   },
