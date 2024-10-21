3731ab45c83110ef4209bdc106ce9a80c44d8a34
Marta Ocana <martacyta2021@gmail.com>
2024-10-21 14:35:06 +0200

    update about-page, contact-page and pricing-page component

--- src/app/pages/about/about-page.component.ts
+++ src/app/pages/about/about-page.component.ts
@@ -1,5 +1,6 @@
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector:'page-about',
@@ -9,4 +10,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './about-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AboutPageComponent { }
+export default class AboutPageComponent implements OnInit{
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('About Page');
+    this.meta.updateTag({name: 'description', content: 'Este es mi About Page'});
+    this.meta.updateTag({name: 'og:title', content: 'About Page'});
+    this.meta.updateTag({name: 'keywords', content: 'Hola, Mundo, Marta, Ocaña'});
+  };
+
+ }
--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,5 +1,6 @@
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-pricing',
@@ -9,4 +10,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent { }
+export default class PricingPageComponent implements OnInit{
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    console.log(this.platform);
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({name: 'description', content: 'Este es mi Pricing Page'});
+    this.meta.updateTag({name: 'og:title', content: 'Pricing Page'});
+    this.meta.updateTag({name: 'keywords', content: 'Hola, Mundo, Marta, Ocaña'});
+  };
+
+ }
