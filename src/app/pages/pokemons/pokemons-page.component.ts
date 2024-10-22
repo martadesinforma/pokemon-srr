548a7b615ec404f0fac1ffc1cb4e5d2194525f1e
Marta Ocana <martacyta2021@gmail.com>
2024-10-22 18:38:16 +0200

    update pokemons-page component

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,11 +1,13 @@
 
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
 
 @Component({
   selector: 'pokemons-page',
   standalone: true,
   imports: [
-  ],
+    PokemonListComponent
+],
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
