4a4f094525d9444be45f9274bef0aab4b4d16608
Marta Ocana <martacyta2021@gmail.com>
2024-10-22 19:47:28 +0200

    update pokemons-page component

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,14 +1,26 @@
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
+import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 
 @Component({
   selector: 'pokemons-page',
   standalone: true,
   imports: [
-    PokemonListComponent
+    PokemonListComponent,
+    PokemonListSkeletonComponent
 ],
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonsPageComponent { }
+export default class PokemonsPageComponent implements OnInit{
+
+  public isLoading = signal(true);
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading.set(false)
+    }, 5000);
+  }
+
+}
