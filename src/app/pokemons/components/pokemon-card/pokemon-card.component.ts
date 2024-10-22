6ea53fb5464d1cf57939d4128e60f43e7b96d507
Marta Ocana <martacyta2021@gmail.com>
2024-10-22 18:38:55 +0200

    create and update pokemon-card component

---
+++ src/app/pokemons/components/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,13 @@
+
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'pokemon-card',
+  standalone: true,
+  imports: [
+
+  ],
+  templateUrl: './pokemon-card.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PokemonCardComponent { }
