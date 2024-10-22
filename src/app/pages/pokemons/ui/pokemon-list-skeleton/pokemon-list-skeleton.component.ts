b9cf438f1e855a5037d7dc6611ce49a45287ca20
Marta Ocana <martacyta2021@gmail.com>
2024-10-22 19:46:47 +0200

    create and update pokemon-list-skeleton component

---
+++ src/app/pages/pokemons/ui/pokemon-list-skeleton/pokemon-list-skeleton.component.ts
@@ -0,0 +1,13 @@
+
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'pokemon-list-skeleton',
+  standalone: true,
+  imports: [
+
+  ],
+  templateUrl: './pokemon-list-skeleton.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PokemonListSkeletonComponent { }
