293832464b6e3c5a5151ce2fe563d200673ba4c8
Marta Ocana <martacyta2021@gmail.com>
2024-10-24 17:48:06 +0200

    create pokemons-page component

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,7 +1,9 @@
 
-import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
 import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
+import { PokemonsService } from '../../pokemons/services/pokemons.service';
+import { SimplePokemon } from '../../pokemons/interfaces/simple-pokemon.interface';
 
 @Component({
   selector: 'pokemons-page',
@@ -15,12 +17,20 @@ import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon
 })
 export default class PokemonsPageComponent implements OnInit{
 
-  public isLoading = signal(true);
+  private pokemonsService = inject(PokemonsService);
+  public pokemons = signal<SimplePokemon[]>([]);//pokemons es un array de  simplePokemons. Voy a pasarle esta señal de pokemons a mi componente pokemon-list  mediante un input.
 
+
+  //Durante SSR, Angular espera a que todas las tareas dentro de ngOnInit y otros ciclos de vida sean completadas antes de considerar la aplicación en modo "stable". Solo cuando el ngOnInit ha terminado y Angular ya no espera más eventos asincrónicos o promesas, considera que el componente ha alcanzado el estado "stable" y entonces genera el HTML final para enviarlo al cliente.
   ngOnInit(): void {
-    setTimeout(() => {
-      this.isLoading.set(false)
-    }, 5000);
+    this.loadPokemons();
   }
 
+  public loadPokemons(page = 0) {
+    this.pokemonsService.loadPage(page)
+      .subscribe(pokemons => { //pokemons es un array de  simplePokemons que luce de esta manera [{id: '1', name: 'bulbasaur'}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+        this.pokemons.set(pokemons);
+        console.log('desde el navegador', pokemons);
+      })
+  }
 }
