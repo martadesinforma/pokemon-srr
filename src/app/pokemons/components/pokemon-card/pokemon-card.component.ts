4f359d92f19a8835994f4b60fdf9a90c9d4db249
Marta Ocana <martacyta2021@gmail.com>
2024-10-24 17:48:32 +0200

    create pokemon-card component

--- src/app/pokemons/components/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemons/components/pokemon-card/pokemon-card.component.ts
@@ -1,5 +1,6 @@
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, input } from '@angular/core';
+import { SimplePokemon } from '../../interfaces/simple-pokemon.interface';
 
 @Component({
   selector: 'pokemon-card',
@@ -10,4 +11,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pokemon-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PokemonCardComponent { }
+export class PokemonCardComponent {
+
+  public pokemon = input.required<SimplePokemon>(); //va a recibir un  simplePokemon del componente pokemon-list. En el pokemon-list.component.html hacemos la asociación:<pokemon-card [pokemon]="pokemon"></pokemon-card>. Este input lo estoy recibiendo directamente del pokemon-list.component.html, no del pokemon-list.component.ts Ahora vamos a hacer uso de este pokemon en el pokemon-card.component.html
+
+  public readonly pokemonImage = computed(
+    () =>
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.pokemon().id}.png`
+  )
+
+
+}
