e5d51cc230eaf7468291c2ebfd81cc987d0ac0c1
Marta Ocana <martacyta2021@gmail.com>
2024-10-25 13:58:59 +0200

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: { //Esta modificación en el archivo tailwind.config.js permite agregar una animación personalizada llamada fadeIn a tu proyecto. Luego vamos a conseguir agregar esta animacion escribiendo la clase animate-fadeIn en el selector que quiera, en este caso, en el div del pokemon-card.component.html
+      keyframes: {
+        fadeIn: {
+          '0%': {opacity: 0},
+          '100%': {opacity: 1},
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn .2s ease-in-out'
+      }
+    },
   },
   plugins: [],
 }
