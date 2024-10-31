ea94178599cad9a4d6939c9d69f3d8b9e7e63cc7
Marta Ocana <martacyta2021@gmail.com>
2024-10-31 12:14:40 +0100

    update pokemon-card.component.spec.ts

--- src/app/pokemons/components/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/pokemons/components/pokemon-card/pokemon-card.component.spec.ts
@@ -24,10 +24,10 @@ describe('PokemonCardComponent', () => { //describe es un agrupador de pruebas. 
       ]
     }).compileComponents();
     fixture = TestBed.createComponent(PokemonCardComponent);
-    fixture.componentRef.setInput('pokemon', mockPokemon);
+    fixture.componentRef.setInput('pokemon', mockPokemon); //Esta línea es clave. Establece el input del componente para que pokemon sea igual a mockPokemon. De esta manera, mockPokemon se convierte en el dato que el componente recibe y usa, imitando la interacción de la aplicación real.
     compiled = fixture.nativeElement as HTMLElement;
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); //Fuerza la detección de cambios para actualizar el DOM del componente con los datos proporcionados.
   });
 
   //esta prueba nos va a ayudar a nosotros a saber si la aplicación o nuestro componente se logra montar.Esto ayuda a saber si la inyección de dependencias está correcto
@@ -35,4 +35,30 @@ describe('PokemonCardComponent', () => { //describe es un agrupador de pruebas. 
     console.log(compiled);
     expect(component).toBeTruthy();
   });
+
+
+  // Esta prueba verifica que el componente tiene correctamente asignada la señal pokemon
+  it('should have the SimplePokemon signal inputValue', () => {
+    expect(component.pokemon()).toEqual(mockPokemon); //toEqual() se utiliza para comparar el **contenido** de objetos o arrays.
+  });
+
+  //Esta prueba verifica que el nombre e imagen del Pokémon se rendericen correctamente en el HTML del componente.
+  it('should render the pokemon name and image correctly', () => {
+    const image = compiled.querySelector('img')!;
+    expect(image).toBeDefined();
+
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${mockPokemon.id}.png`;
+    expect(image.src).toBe(imageUrl);
+    expect(compiled.querySelector('h2')!.textContent?.trim()).toBe(mockPokemon.name); //Verifica que el nombre del Pokémon (mockPokemon.name) esté presente en el texto del componente.
+  });
+
+  //Esta prueba revisa que el enlace de navegación (ng-reflect-router-link) esté correctamente configurado en el HTML.
+  it('should have the proper ng-reflect-router-link', () => {
+    const divWithLink = compiled.querySelector('div');
+
+    expect(
+      divWithLink?.attributes.getNamedItem('ng-reflect-router-link')?.value
+    ).toBe(`/pokemon,${mockPokemon.name}`);
+  });
+
 });
